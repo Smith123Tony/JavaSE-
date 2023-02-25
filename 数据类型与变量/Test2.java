8b4e4db3332ce4d3c4ffbe1afd78c216eb8960c8
Smith123Tony <JK.Tony@outlook.com>
2023-02-25 21:36:22 +0800

    Java数据类型与变量，以及类型转换。

---
+++ 数据类型与变量/Test2.java
@@ -0,0 +1,101 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Parallel
+ * Date: 2023-02-23
+ * Time: 20:31
+ */
+public class Test2 {
+    public static void main(String[] args) {   //快捷键：main//psvm
+       /* System.out.println("hello IDEA");      //sout
+        System.out.println("hello world");
+        System.out.println("=========");*/
+        /*int a=10;
+        double b=20;
+        float c=20;
+        char d='A';
+        boolean e=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);*/
+       /* System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);*/
+
+        /*System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);*/
+
+        /*long c=10L;
+        long d=10L;
+        System.out.println();*/
+        /*byte g=10;
+        System.out.println(g);
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);*/
+        /*double a=1.1;
+        System.out.println(a*a);*/
+
+       /* float n=53.2f;
+        System.out.println(n);*/
+
+       /* char ch='a';//注意占用的是2个字节
+        char ch2='好';//也可以表示汉字
+        System.out.println(ch);
+        System.out.println(ch2);*/
+
+        /*boolean flg=true;
+        System.out.println(flg);*/
+
+        /*int a=10;
+        long b=20;
+        b=a;//编译可以通过
+        a=b;//编译不可以通过*/
+
+//        int a=10;
+//        boolean flg=true;
+//        flg=(bollean)a;
+
+//        int a=10;
+//        long b=20;
+//        long c=a+b;
+
+//        byte a=10;
+//        byte b=20;
+//        int c=a+b;
+
+        /*String s="hello";
+        System.out.println(s);*/
+
+       /* int a=10;
+        int b=20;
+        System.out.println(a+b);
+        System.out.println("a:"+a+"b:"+b);*/
+
+        /*int a=10;
+        int b=20;
+        System.out.println("a:"+a+b);
+        System.out.println(a+b+"hello");*/
+
+//        String s1="hello ";
+//        String s2="world";
+//        System.out.println(s1+s2);
+
+//        int num=10;
+//        //方法1
+//        String str1=num+"";
+//        System.out.println(str1);
+//        //方法2
+//        String str2=String.valueOf(num);
+//        System.out.println(str2);
+
+//        String str="1234";
+//        //方法1
+//        int a=Integer.parseInt(str);
+//        System.out.println(a+1);
+//        //方法2
+//        a=Integer.valueOf(str);
+//        System.out.println(a+1);
+    }
+}
